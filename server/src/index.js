d84e7c55e1118ceeb2d67d95b60fd3b25fba154b
aenansh <231030317@juitsolan.in>
2026-07-16 13:02:08 +0530

    server html rendering

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,11 @@
 import app from "./app.js";
 import env from "./utils/env.util.js";
+import path from "path"
 
 const PORT = env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(process.cwd(), "index.html"));
+  res.sendFile(path.join(process.cwd(), "src/index.html"));
 });
 
 app.listen(PORT, () => {
